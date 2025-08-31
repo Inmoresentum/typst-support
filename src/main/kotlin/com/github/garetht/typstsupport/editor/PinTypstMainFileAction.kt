2eb129ca767a401219f03cd5bae9d182844bb163
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2025-08-31 22:32:06 +0600

    feat(wip): added the functionality to send commands to tinymist lsp

--- src/main/kotlin/com/github/garetht/typstsupport/editor/PinTypstMainFileAction.kt
+++ src/main/kotlin/com/github/garetht/typstsupport/editor/PinTypstMainFileAction.kt
@@ -2,10 +2,16 @@ package com.github.garetht.typstsupport.editor
 
 import com.github.garetht.typstsupport.configuration.TypstProjectSettings
 import com.github.garetht.typstsupport.language.filetype.TypstFileType
+import com.github.garetht.typstsupport.languageserver.TypstLspServerSupportProvider
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.platform.lsp.api.LspServerManager
+import org.eclipse.lsp4j.ExecuteCommandParams
+
+private val LOG = logger<PinTypstMainFileAction>()
 
 class PinTypstMainFileAction : AnAction() {
     override fun update(e: AnActionEvent) {
@@ -27,14 +33,26 @@ class PinTypstMainFileAction : AnAction() {
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project = e.project!!
-        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)!!
-        val settings = TypstProjectSettings.getInstance(project)
+        val project = e.project ?: return
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+        LOG.info("Pinning/Unpinning Typst main file: ${virtualFile.name}")
 
-        if (settings.state.mainFilePath == virtualFile.path) {
-            settings.state.mainFilePath = null
-        } else {
-            settings.state.mainFilePath = virtualFile.path
+        val settings = TypstProjectSettings.getInstance(project)
+        val mainFilePath = virtualFile.path
+        val mainFileUri = "file://${mainFilePath}"
+
+        // Toggle pin state
+        settings.state.mainFilePath = if (settings.state.mainFilePath == mainFilePath) null else mainFilePath
+
+        val lspServer = LspServerManager.getInstance(project)
+            .getServersForProvider(TypstLspServerSupportProvider::class.java)
+            .firstOrNull() ?: return
+
+        val command = "tinymist.pinMain"
+        val arguments = if (settings.state.mainFilePath != null) listOf(mainFileUri) else null
+
+        lspServer.sendRequestSync {
+            it.workspaceService.executeCommand(ExecuteCommandParams(command, arguments))
         }
     }
 
